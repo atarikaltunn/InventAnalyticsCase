7785730495a0f4122c8d0ce4f91c9a31621bcedd
atarikaltunn <atarikaltunn@gmail.com>
2022-09-29 09:12:16 +0300

    code beautify edit commit, new comment lines added

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -3,20 +3,26 @@
 const express = require('express');
 const db = require('../config/database');
 const Book = require('../models/Book');
-const bookController = require('../controller/bookController')
+const bookController = require('../controller/bookController');
 
 const router = express.Router();
 
 //Get Books
-router.route('/').get(bookController.getAllBooks);
+router.route('/').get(
+    bookController.getAllBooks      //function that returns all books with id and name 
+);
 
 //Get Book
-router.route('/:id').get(bookController.getBook);
+router.route('/:id').get(
+    bookController.getBook          //function that returns a book with id, name and score. If has no score, returns -1 as score 
+);
 
 //Create Book
-router.route('/').post(bookController.createBook);
+router.route('/').post(
+    bookController.createBook       //function that creates a book with given name
+);
 
-// router.route('/delete/deleteAll').get(bookController.deleteAllBooks);
-
+//Delete All Books
+// router.route('/delete/deleteAll').get(bookController.deleteAllBooks);       //function to use while testing
 
 module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,20 +6,24 @@ const db = require('../config/database');
 const User = require('../models/User');
 const Book = require('../models/Book');
 const userController = require('../controller/userController');
-const userExistsMiddleware = require('../middlewares/userExistsMiddleware');
-const isBookTakenMiddleware = require('../middlewares/isBookTakenMiddleware');
-const isBookNotTakenMiddleware = require('../middlewares/isBookNotTakenMiddleware');
-const userHasBookMiddleware = require('../middlewares/userHasBookMiddleware');
-const userNotHasBookMiddleware = require('../middlewares/userNotHasBookMiddleware');
-const isReturnerTheOwner = require('../middlewares/isReturnerTheOwner');
+const userExists = require('../middlewares/userExistsMiddleware');
+const isBookTaken = require('../middlewares/isBookTakenMiddleware');
+const isBookNotTaken = require('../middlewares/isBookNotTakenMiddleware');
+const userHasBook = require('../middlewares/userHasBookMiddleware');
+const userNotHasBook = require('../middlewares/userNotHasBookMiddleware');
+const isReturnerTheBorrower = require('../middlewares/isReturnerTheBorrowerMiddleware');
 
 const router = express.Router();
 
 //Get All Users
-router.route('/').get(userController.getAllUsers);
+router.route('/').get(
+    userController.getAllUsers  //function that returns all users with id and name
+);
 
 //Get User
-router.route('/:id').get(userController.getUser);
+router.route('/:id').get(
+    userController.getUser      //funciton that returns a user with id, name and past and present history if has
+);
 
 //Create User
 router.route('/').post(
@@ -40,10 +44,9 @@ router.route('/').post(
         next();
     },
     // validation to check name characters
-    body(
-        'name',
-        'Name only can contain letters and spaces' 
-    ).matches(/^[A-Za-zğüşöçıİĞÜŞÖÇ\s]+$/),
+    body('name', 'Name only can contain letters and spaces').matches(
+        /^[A-Za-zğüşöçıİĞÜŞÖÇ\s]+$/
+    ),
     (req, res, next) => {
         // Finds the validation errors in this request and wraps them in an object with handy functions
         const errors = validationResult(req);
@@ -56,21 +59,20 @@ router.route('/').post(
 );
 
 //Borrow Book
-router
-    .route('/:id/borrow/:bookid')
-    .post(
-        userExistsMiddleware,
-        userHasBookMiddleware,
-        isBookTakenMiddleware,
-        userController.borrowBook
-    );
+router.route('/:id/borrow/:bookid').post(
+    userExists,                 //if user exists then user can borrow a book
+    userHasBook,                //if user has a book then user can not take a new book without returning that book
+    isBookTaken,                //if a book is borrowed before and not returned then a user can not borrow it again
+    userController.borrowBook   //borrow book function that where database updates are happen
+);
 
 //Return Book
 router.route('/:id/return/:bookid').post(
-    userExistsMiddleware,
-    userNotHasBookMiddleware,
-    isBookNotTakenMiddleware,
-    isReturnerTheOwner,
+    userExists,             //if user exists so can return book
+    userNotHasBook,         //if user do not have any book then can not return a book
+    isBookNotTaken,         //if book is not taken from the library before then the book can not be returnable
+    isReturnerTheBorrower,  //if returner is different from book borrower then returner is not able to return that book
+
     //score validator that checks score is between 0-10
     body('score', 'Score must be a number between 0 and 10').isInt({
         min: 0,
@@ -85,10 +87,10 @@ router.route('/:id/return/:bookid').post(
         next();
     },
 
-    userController.returnBook
+    userController.returnBook // return book function that where db updates are happen
 );
 
 //Delete All Users
-// router.route('/delete/deleteAll').get();
+// router.route('/delete/deleteAll').get(); //function to use while testing
 
 module.exports = router;
